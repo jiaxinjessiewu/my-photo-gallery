ff5f4dac944a1fdf458c28555742dd28b82c40a0
JessieWu <49457922+jiaxinjessiewu@users.noreply.github.com>
2020-04-30 00:17:28 -0400

    gallery uploader form html #time 30m

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GalleryComponent } from './gallery/gallery.component';
+import { UploaderComponent } from './uploader/uploader.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/gallery', pathMatch: 'full'},
+  { path: 'gallery', component: GalleryComponent},
+  { path: 'upload', component: UploaderComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
