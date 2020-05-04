89a8c6a8653926678391115951eb801ca5c7e64a
JessieWu <49457922+jiaxinjessiewu@users.noreply.github.com>
2020-05-04 09:04:05 -0400

    add search component #time 10m

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GalleryComponent } from './gallery/gallery.component';
 import { UploaderComponent } from './uploader/uploader.component';
+import { SearchComponent } from './search/search.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/gallery', pathMatch: 'full'},
   { path: 'gallery', component: GalleryComponent},
-  { path: 'upload', component: UploaderComponent}
+  { path: 'upload', component: UploaderComponent},
+  { path: 'search', component: SearchComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,19 +9,31 @@ import { GalleryComponent } from './gallery/gallery.component';
 import { HeaderComponent } from './header/header.component';
 import { UploaderComponent } from './uploader/uploader.component';
 import { GalleryService } from './service/gallery.service';
+import { SearchComponent } from './search/search.component';
+
+/* 
+jessie-gallery-flickr
+Key:
+d6b7d34c02597060ea986e43a19ddcbe
+
+Secret:
+0496e2e8ebcf147e
+*/
 
 @NgModule({
   declarations: [
     AppComponent,
     GalleryComponent,
     HeaderComponent,
-    UploaderComponent
+    UploaderComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [GalleryService],
   bootstrap: [AppComponent]
